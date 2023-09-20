3355b67ff07c252564f464c2580041afe6d5cfda
creativetemmy <temitopeayo01@gmail.com>
2023-09-20 15:42:00 +0100

    Added contents to backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-import GlobalStyles from "./styles/GlobalStyles";
-import Dashboard from "./pages/Dashboard";
-import Bookings from "./pages/Bookings";
-import Cabins from "./pages/Cabins";
-import Users from "./pages/Users";
-import Settings from "./pages/Settings";
-import Account from "./pages/Account";
-import Login from "./pages/Login";
-import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./ui/AppLayout";
+import GlobalStyles from './styles/GlobalStyles'
+import Dashboard from './pages/Dashboard'
+import Bookings from './pages/Bookings'
+import Cabins from './pages/Cabins'
+import Users from './pages/Users'
+import Settings from './pages/Settings'
+import Account from './pages/Account'
+import Login from './pages/Login'
+import PageNotFound from './pages/PageNotFound'
+import AppLayout from './ui/AppLayout'
+import { Toaster } from 'react-hot-toast'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // staleTime: 60 * 1000,
+      staleTime: 0,
+    },
+  },
+})
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -31,8 +45,25 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
-  );
+
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: '8px' }}
+        toastOptions={{
+          success: { duration: 3000 },
+          error: { duration: 3000 },
+          style: {
+            fontSize: '16px',
+            maxWidth: '500px',
+            padding: '16px 24px',
+            backgroundColor: 'var(--color-grey-0)',
+            color: 'var(--color-grey-700)',
+          },
+        }}
+      />
+    </QueryClientProvider>
+  )
 }
 
-export default App;
+export default App
--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,12 +1,12 @@
 // import styled from 'styled-components';
-import CabinRow from 'features/cabins/CabinRow';
-import Spinner from 'ui/Spinner';
-import Table from 'ui/Table';
-import Menus from 'ui/Menus';
-import Empty from 'ui/Empty';
-import { useCabins } from 'features/cabins/useCabins';
-import { useSearchParams } from 'react-router-dom';
-import { Suspense } from 'react';
+import CabinRow from 'features/cabins/CabinRow'
+import Spinner from 'ui/Spinner'
+import Table from 'ui/Table'
+import Menus from 'ui/Menus'
+import Empty from 'ui/Empty'
+import { useCabins } from 'features/cabins/useCabins'
+import { useSearchParams } from 'react-router-dom'
+import { Suspense } from 'react'
 
 // v2
 // Right now this is not really reusable... But we will want to use a similar table for guests as well, but with different columns. ALSO, right now we are defining these columns in BOTH the TableHeader and the CabinRow, which is not good at all. Instead, it would be much better to simply pass the columns into the Table, and the table would give access to the columns to both the header and row. So how can we do that? Well we can again use a compound component! We don't HAVE to do it like this, there's a million ways to implement a table, also without CSS Grid, but this is what I chose
@@ -40,39 +40,39 @@ function CabinTable() {
 
   // Now, everything that's inside a Suspense will be treated as just one unit, so when just one component of the child components is currently suspended, all of them will be replaced with the fallback. We can nest multiple Suspense boundaries, and the closest one will be shown. This way, when we have a big Suspense on the top on the tree, it won't have to WAIT
 
-  const { cabins } = useCabins();
-  const [searchParams] = useSearchParams();
+  const { cabins } = useCabins()
+  const [searchParams] = useSearchParams()
 
   // if (isLoading) return <Spinner />;
   // if (!cabins) return <Empty resource={'cabins'} />;
 
   // 1) Filter
-  const filterValue = searchParams.get('discount') || 'all';
+  const filterValue = searchParams.get('discount') || 'all'
 
   // This is probably not the most efficient way, but that doesn't matter
-  let filteredCabins;
-  if (filterValue === 'all') filteredCabins = cabins;
+  let filteredCabins
+  if (filterValue === 'all') filteredCabins = cabins
   if (filterValue === 'no-discount')
-    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0)
   if (filterValue === 'with-discount')
-    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0)
 
   // 2) SortBy
-  const sortBy = searchParams.get('sortBy') || 'startDate-asc';
-  const [field, direction] = sortBy.split('-');
-  const modifier = direction === 'asc' ? 1 : -1;
+  const sortBy = searchParams.get('sortBy') || 'startDate-asc'
+  const [field, direction] = sortBy.split('-')
+  const modifier = direction === 'asc' ? 1 : -1
 
   // This one is better!
   // .sort((a, b) => a[field].localeCompare(b[field]) * modifier);
 
   const sortedCabins = filteredCabins.sort(
     (a, b) => (a[field] - b[field]) * modifier
-  );
+  )
 
   return (
     <Menus>
       {/* A beautiful API we created here! We could even have defined the widths on the columns in the table header individually, but this keeps it simpler, and I also really like it */}
-      <Table columns='9.6rem 0.8fr 2fr 1fr 1fr 3.2rem'>
+      <Table columns="9.6rem 0.8fr 2fr 1fr 1fr 3.2rem">
         <Table.Header>
           <div></div>
           <div>Cabin</div>
@@ -93,10 +93,10 @@ function CabinTable() {
         />
       </Table>
     </Menus>
-  );
+  )
 }
 
 // We could create yet another layer of abstraction on top of this. We could call this component just <Results>, like: Results({data, count, isLoading, columns, rowComponent}). Then <CabinTable> and ALL other tables would simply call that.
 // BUT, creating more abstractions also has a cost! More things to remember, more complex codebase to understand. Sometimes it's okay to just copy and paste instead of creating abstractions
 
-export default CabinTable;
+export default CabinTable
